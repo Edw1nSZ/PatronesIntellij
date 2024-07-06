2cd4900718e56b0ce4d4cbdc4270fc58b3eb8221
Edw1nSZ <edwincruz2basico@gmail.com>
2024-07-06 12:28:37 -0600

    Singleton Subida 2

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,7 +1,18 @@
 package org.example;
 
+import org.example.config.AppConfig;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        AppConfig config = AppConfig.getInstance();
+        config.setServerUrl("http://ApiLocal.com");
+        // TODO: Utiliza más propiedades de la configuración aquí
+        //pruebas de otras funciones como si fuera un servidor
+        System.out.println("\nURL del Servidor: " + config.getServerUrl());
+        System.out.println("\nPuerto: " + config.getPort());
+        System.out.println("\nURL de la Base de Datos: " + config.getDatabaseUrl());
+        System.out.println("Ha sido conectado Correctamente!!");
+
     }
 }
--- src/main/java/org/example/config/AppConfig.java
+++ src/main/java/org/example/config/AppConfig.java
@@ -1,10 +1,48 @@
-package org.example;
+package org.example.config;
 
 public class AppConfig {
     private static AppConfig instance;
+    private String serverUrl;
+    private int port;
+    private String databaseUrl;
 
     private AppConfig() {
         // TODO: Inicializa tu configuración aquí si es necesario
-        
+        serverUrl = "http://UrlpruebaAda.com";
+        //ejemplo de un puerto cualquiera
+        port = 1501;
+        databaseUrl = "http://localhost:1501/users";
+       // apiKey = "default-api-key";
+    }
+
+    public static synchronized AppConfig getInstance() {
+        if (instance == null) {
+            instance = new AppConfig();
+        }
+        return instance;
+    }
+
+    public String getServerUrl() {
+        return serverUrl;
+    }
+
+    public void setServerUrl(String serverUrl) {
+        this.serverUrl = serverUrl;
+    }
+
+    public String getDatabaseUrl() {
+        return databaseUrl;
+    }
+
+    public void setDatabaseUrl(String databaseUrl) {
+        this.databaseUrl = databaseUrl;
+    }
+
+    public int getPort() {
+        return port;
+    }
+
+    public void setPort(int port) {
+        this.port = port;
     }
 }
