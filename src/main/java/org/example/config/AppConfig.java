cd0cf4695ab5c9af216887ab2ad11253fb527499
Edw1nSZ <edwincruz2basico@gmail.com>
2024-07-06 12:14:27 -0600

    Singleton

---
+++ src/main/java/org/example/config/AppConfig.java
@@ -0,0 +1,10 @@
+package org.example;
+
+public class AppConfig {
+    private static AppConfig instance;
+
+    private AppConfig() {
+        // TODO: Inicializa tu configuración aquí si es necesario
+        
+    }
+}
